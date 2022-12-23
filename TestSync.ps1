9d8340f2afec320490f9c1f3763c36a5b905c806
REKHU CHINNARATHOD <vrchinnarathod@gmail.com>
2022-12-23 11:52:34 +0000

    Added TestSync.ps1

---
+++ TestSync.ps1
@@ -0,0 +1,65 @@
+param(
+     [Parameter()]
+     [string]$GitHubDestinationPAT,
+ 
+     [Parameter()]
+     [string]$ADOSourcePAT
+ )
+
+# Write your PowerShell commands here.
+Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
+Write-Host ' reflect Azure Devops repo changes to GitHub repo'
+Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
+$AzureRepoName = "Docker"
+Write-Host "AzureRepoName: $AzureRepoName"
+$GitHubUserName = "RekhuGopal"
+$ADOCloneURL = "dev.azure.com/CloudQuickLabs/DockerImageAzureWebApp/_git/Docker"
+$GitHubCloneURL = "github.com/RekhuGopal/Docker.git"
+$stageDir = pwd | Split-Path
+Write-Host "stage Dir is : $stageDir"
+$githubDir = $stageDir +"\"+"gitHub"
+Write-Host "github Dir : $githubDir"
+$destination = $githubDir+"\"+ $AzureRepoName+".git"
+Write-Host "destination: $destination"
+#please provide your username
+$alias = $GitHubUserName+":"+ "$($GitHubDestinationPAT)"
+write-host "Alias : $alias"
+#Please make sure, you remove https from azure-repo-clone-url
+$sourceURL = "https://$($ADOSourcePAT)"+"@"+"$($ADOCloneURL)"
+write-host "source URL : $sourceURL"
+#Please make sure, you remove https from github-repo-clone-url
+$destURL = "https://" + $($GitHubDestinationPAT) +"@"+"$($GitHubCloneURL)"
+$destURLSetURL = "https://" + $($alias) +"@"+"$($GitHubCloneURL)"
+write-host "dest URL : $destURL"
+#Check if the parent directory exists and delete
+if((Test-Path -path $githubDir))
+{
+  Remove-Item -Path $githubDir -Recurse -force
+}
+if(!(Test-Path -path $githubDir))
+{
+  New-Item -ItemType directory -Path $githubDir
+  Set-Location $githubDir
+  git clone --mirror $sourceURL
+}
+else
+{
+  Write-Host "The given folder path $githubDir already exists";
+}
+Set-Location $destination
+Write-Output '*****Git removing remote secondary****'
+git remote rm secondary
+Write-Output '*****Git remote add****'
+git remote add --mirror=fetch secondary $destURL
+Write-Output '*****Git fetch origin****'
+git fetch $sourceURL
+Write-Output '*****Git push secondary****'
+#git remote set-url origin $destURLSetURL
+git push secondary  --all -f
+Write-Output '**Azure Devops repo synced with Github repo**'
+Set-Location $stageDir
+if((Test-Path -path $githubDir))
+{
+ Remove-Item -Path $githubDir -Recurse -force
+}
+write-host "Job completed"
